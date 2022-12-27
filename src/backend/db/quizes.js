106b574272bd4fdff12d657cbaa0bc7595533e2a
mnath30 <maitreyeenath30798@gmail.com>
2022-12-27 19:28:07 +0530

    chore: Added quiz data

--- src/backend/db/quizes.js
+++ src/backend/db/quizes.js
@@ -61,6 +61,67 @@ export const quizes = [
     ],
   },
   {
+    id: "ha6rrygHRES_Pttr",
+    title: "Harry Potter",
+    description: "Are you a potter head?",
+    category: "Movies",
+    numberOfQuestions: 5,
+    imgUrl:
+      "https://res.cloudinary.com/dmb5mqtbx/image/upload/v1672147573/Harry_Potter_hjwdm6.jpg",
+    questionList: [
+      {
+        question:
+          "Philosopher's Stone: Who does Harry meet first out of these?",
+        options: [
+          { value: "Molly", isCorrect: false },
+          { value: "Hagrid", isCorrect: true },
+          { value: "Quirrel", isCorrect: false },
+          { value: "Prof. Snape", isCorrect: false },
+        ],
+      },
+      {
+        question: "Who teaches Harry the rules of Quidditch?",
+        options: [
+          { value: "Fred and George Weasely", isCorrect: false },
+          {
+            value: "Ron Weasely",
+            isCorrect: false,
+          },
+          { value: "Dean Thomas", isCorrect: false },
+          { value: "Oliver Wood", isCorrect: true },
+        ],
+      },
+      {
+        question: "What is Hagrid's three-headed dog called?",
+        options: [
+          { value: "Fang", isCorrect: false },
+          { value: "Fluffy", isCorrect: true },
+          { value: "Floppy", isCorrect: false },
+          { value: "Nobert", isCorrect: false },
+        ],
+      },
+      {
+        question: "What spell does Hermione first cast in the movie?",
+        options: [
+          { value: "Alohomora", isCorrect: false },
+          { value: "Lumos", isCorrect: false },
+          { value: "Oculus Reparo ", isCorrect: true },
+          { value: "Wingardium Leviosa", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "What kind of monster is released from the Chamber of Secrets?",
+        options: [
+          { value: "Voldemort", isCorrect: false },
+          { value: "Basilisk", isCorrect: true },
+          { value: "Acromantula", isCorrect: false },
+          { value: "Boggart", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
     id: "fe5dMEP_Sui",
     title: "Suits",
     description: "Do you think you know all about Suits? Let's check",
@@ -112,6 +173,194 @@ export const quizes = [
           { value: "Jessica", isCorrect: false },
           { value: "Norma", isCorrect: true },
           { value: "Harvey", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
+    id: "fMonkfgsWES_eerA",
+    title: "Monk who sold his ferrari",
+    description: "Have you read The monk who sold his ferrari?",
+    category: "Books",
+    numberOfQuestions: 5,
+    imgUrl:
+      "https://res.cloudinary.com/dmb5mqtbx/image/upload/v1672148634/MonkFerrari_lgdn1t.jpg",
+    questionList: [
+      {
+        question: "What is the surname of the main character?",
+        options: [
+          { value: "Mettle", isCorrect: false },
+          { value: "Mattel", isCorrect: false },
+          { value: "Mantle", isCorrect: true },
+          { value: "Mental", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "What is the profession of the main character in the beginning?",
+        options: [
+          { value: "Attorney", isCorrect: true },
+          {
+            value: "Saint",
+            isCorrect: false,
+          },
+          { value: "Artist", isCorrect: false },
+          { value: "Entrepreneur", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Everything is created twice, first in the mind and then in ______",
+        options: [
+          { value: "imagination", isCorrect: false },
+          { value: "world", isCorrect: false },
+          { value: "heart", isCorrect: false },
+          { value: "reality", isCorrect: true },
+        ],
+      },
+      {
+        question: "What else did Julian sell along with his Ferrari?",
+        options: [
+          { value: "farm house", isCorrect: false },
+          { value: "office", isCorrect: false },
+          { value: "mansion", isCorrect: true },
+          { value: "golf course", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Who is the author of the book 'The Monk Who Sold His Ferrari '",
+        options: [
+          { value: "Rabindranath Tagore", isCorrect: false },
+          { value: "Robin Sharma", isCorrect: true },
+          { value: "Charles Dickens", isCorrect: false },
+          { value: "Stephen King", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
+    id: "fe5hfgsWES_Smi",
+    title: "JavaScript",
+    description: "Let's check your JS knowledge",
+    category: "Web Development",
+    numberOfQuestions: 5,
+    imgUrl:
+      "https://res.cloudinary.com/dmb5mqtbx/image/upload/v1672143935/JavaScript_uhfwt9.png",
+    questionList: [
+      {
+        question: "How can a datatype be declared to be a constant type?",
+        options: [
+          { value: "const", isCorrect: true },
+          { value: "var", isCorrect: false },
+          { value: "let", isCorrect: false },
+          { value: "constant", isCorrect: false },
+        ],
+      },
+      {
+        question: "What is the use of the <noscript> tag in Javascript?",
+        options: [
+          { value: "Clears all the cookies and cache", isCorrect: false },
+          {
+            value: "The contents are displayed by non-JS based browsers",
+            isCorrect: true,
+          },
+          { value: "Both of the above", isCorrect: false },
+          { value: "None of the above", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "What keyword is used to check whether a given property is valid or not?",
+        options: [
+          { value: "in", isCorrect: true },
+          { value: "is in", isCorrect: false },
+          { value: "exists", isCorrect: false },
+          { value: "lies", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        options: [
+          { value: "Boolean", isCorrect: false },
+          { value: "undefined", isCorrect: false },
+          { value: "object", isCorrect: true },
+          { value: "Integer", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "What is the output of the following code snippet? \n print(NaN === NaN)",
+        options: [
+          { value: "true", isCorrect: false },
+          { value: "false", isCorrect: true },
+          { value: "undefined", isCorrect: false },
+          { value: "error", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
+    id: "Pa6kUgsRES_CiS",
+    title: "CSS",
+    description: "Let's check your CSS knowledge",
+    category: "Web Development",
+    numberOfQuestions: 5,
+    imgUrl:
+      "https://res.cloudinary.com/dmb5mqtbx/image/upload/v1672143935/CSS_syq3nl.png",
+    questionList: [
+      {
+        question:
+          'Which of the following selector selects all paragraph elements whose lang attribute has a value of exactly "fr"?',
+        options: [
+          { value: "p[lang]", isCorrect: false },
+          { value: 'p[lang="fr"]', isCorrect: true },
+          { value: 'p[lang~="fr"]', isCorrect: false },
+          { value: 'p[lang|="fr"]', isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Which of the following property specifies the distance between a marker and the text in the list?",
+        options: [
+          { value: "list-style", isCorrect: false },
+          {
+            value: "marker-offset",
+            isCorrect: true,
+          },
+          { value: "list-style-image", isCorrect: false },
+          { value: "list-decoration", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Which of the below is the correct syntax to put a line over text in CSS?",
+        options: [
+          { value: "text-decoration:line", isCorrect: false },
+          { value: "text-decoration:overline", isCorrect: true },
+          { value: "text-decoration:underline", isCorrect: false },
+          { value: "text-decoration:none", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Which below property of CSS is used to set the indentation of the first line in a block of text ?",
+        options: [
+          { value: "text-indent property", isCorrect: true },
+          { value: "text-underline property", isCorrect: false },
+          { value: "text-decoration none ", isCorrect: false },
+          { value: "text-overflow property", isCorrect: false },
+        ],
+      },
+      {
+        question:
+          "Which of the below CSS properties represent the order of flex items in the grid container ?",
+        options: [
+          { value: "order", isCorrect: true },
+          { value: "float", isCorrect: false },
+          { value: "overflow", isCorrect: false },
+          { value: "All of the above", isCorrect: false },
         ],
       },
     ],
