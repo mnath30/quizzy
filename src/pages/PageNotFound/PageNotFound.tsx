570d6cce0c10250ddc3cdde10bc1113c057bfdf3
mnath30 <maitreyeenath30798@gmail.com>
2022-12-27 16:20:34 +0530

    feat: Added page not found

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,26 @@
-const PageNotFound = () => {
-  return <h1>Pahe Not Found</h1>;
+import "./page-not-found.css";
+import { Link } from "react-router-dom";
+import { Navigation } from "../../components";
+import { pageNotFound } from "../../assets";
+
+const PageNotFound = (): JSX.Element => {
+  return (
+    <div className="container">
+      <Navigation />
+      <div className="container__content flex-col">
+        <h1>Page Not Found</h1>
+        <div className="container__content--img">
+          <img className="img" alt="page-not-found" src={pageNotFound} />
+        </div>
+        <p>
+          Oops!Could not find the page you were looking for.{" "}
+          <Link to="/categories" className="link" replace={true}>
+            Go to Categories
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export { PageNotFound };
